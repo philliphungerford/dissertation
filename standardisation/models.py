6c242bff12ccff4744dadddd4a2c3523d31be420
philliphungerford <phillip.hungerford@gmail.com>
2019-07-29 14:46:10 +1000

    added pointnet to models

--- standardisation/models.py
+++ standardisation/models.py
@@ -44,6 +44,7 @@ seeds = 0
 # CNN model
 # ==============================================================================
 def CNN(X_train, X_test, y_train, y_test, k, my_tags):
+    print("---------------------------- CNN ---------------------------------")
     # Hyper parameters ---------------------------------------------------------
     max_epochs = 25
     batch_size = 128
@@ -144,6 +145,7 @@ def CNN(X_train, X_test, y_train, y_test, k, my_tags):
 # RNN model
 # ==============================================================================
 def rnn(X_train, X_test, Y_train, Y_test, k, my_tags):
+    print("---------------------------- RNN ---------------------------------")
     print("Building Document Classifier... \n")
     # 0. Hyperparameters -------------------------------------------------------
     # The maximum number of words to be used
@@ -176,6 +178,7 @@ def rnn(X_train, X_test, Y_train, Y_test, k, my_tags):
     X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.10, random_state = 42)
     print(X_train.shape,Y_train.shape)
     print(X_test.shape,Y_test.shape)
+
     '''
     # 3. Build the model -------------------------------------------------------
     model = Sequential()
@@ -192,7 +195,6 @@ def rnn(X_train, X_test, Y_train, Y_test, k, my_tags):
                         callbacks=[EarlyStopping(monitor='val_loss',
                                                  patience=3, min_delta=0.0001)])
 
-    model.save('rnn_model.h5')
     # 4. Results ---------------------------------------------------------------
     print("###################################################################")
     print("\nResults:\n")
@@ -211,25 +213,31 @@ def rnn(X_train, X_test, Y_train, Y_test, k, my_tags):
     plt.plot(history.history['val_acc'], label='test')
     plt.legend()
     plt.show();
-
-    # Make predictions
+    
+    # Make predictions 
     y_pred = model.predict(X_test)
-    report = classification_report(y_test, y_pred.round(), target_names=my_tags)
+    report = classification_report(Y_test, y_pred.round(), target_names=my_tags)
     print("\nClassfication Report for test:\n", report)
     print("###################################################################")
-
-    return (model)
+    
+    return(model)
 
 
 # ==============================================================================
 # Combined model
 # ==============================================================================
 def standardisation_model(X_train_cnn, X_train_rnn, X_test_cnn, X_test_rnn, y_train, y_test, k, my_tags):
-    # Hyperparameters ----------------------------------------------------------
+    print("------------------------- 3DCNN + RNN ----------------------------")
+    # Hyperparameters ---------------------------------------------------------
     dropout_rate = 0.5
     epochs = 25
     batch_size = 128
-
+    size = 16
+    h,w,d = size
+    c = 1
+    MAX_NB_CHARS = 26
+    MAX_SEQUENCE_LENGTH = 5
+    # Vision model ------------------------------------------------------------
     # First, let's define a vision model using a Sequential model.
     # This model will encode an image into a vector.
     vision_model = Sequential()
@@ -320,7 +328,7 @@ def standardisation_model(X_train_cnn, X_train_rnn, X_test_cnn, X_test_rnn, y_tr
 # KNN Reference model
 # ==============================================================================
 
-def knn(X, y):
+def knn(X, y, my_tags):
     '''
     This is a reference model for the dissertation-standardisation task
     The dimensionality for the feature set currently is (1716, 4096)
@@ -331,7 +339,6 @@ def knn(X, y):
     print("Building KNN Reference Model")
 
     # Dependencies ------------------------------------------------------------
-    import numpy as np
     from sklearn.metrics import classification_report
     # Load data ---------------------------------------------------------------
     print("Loading data...")
@@ -340,7 +347,6 @@ def knn(X, y):
 
     # Split the data ----------------------------------------------------------
     print("Splitting the data...")
-    from sklearn.model_selection import train_test_split
     # split data into 1: train+validation set and 2: test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seeds, test_size=0.25)
 
@@ -361,3 +367,224 @@ def knn(X, y):
     print("\nClassfication Report for test:\n", report)
     print("\n####################################################################")
     return (knn, report)
+	
+# ==========================================================================
+# PointNet Full Model
+# ==========================================================================	
+
+import random
+import numpy as np
+import tensorflow as tf
+from numpy.random import seed
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from tensorflow import set_random_seed
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve, classification_report
+from keras.layers import Dense, MaxPooling1D, Convolution1D, Dropout, Flatten, BatchNormalization, Reshape, Lambda
+from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
+
+def mat_mul(A, B):
+    return tf.matmul(A, B)
+
+    # Rotate and jitter points
+def rotate_point_cloud(batch_data):
+    """ Randomly rotate the point clouds to augument the dataset
+        rotation is per shape based along up direction
+        Input:
+          BxNx3 array, original batch of point clouds
+        Return:
+          BxNx3 array, rotated batch of point clouds
+    """
+    rotated_data = np.zeros(batch_data.shape, dtype=np.float32)
+    for k in range(batch_data.shape[0]):
+        rotation_angle = np.random.uniform() * 2 * np.pi
+        cosval = np.cos(rotation_angle)
+        sinval = np.sin(rotation_angle)
+        rotation_matrix = np.array([[cosval, 0, sinval],
+                                    [0, 1, 0],
+                                    [-sinval, 0, cosval]])
+        shape_pc = batch_data[k, ...]
+        rotated_data[k, ...] = np.dot(shape_pc.reshape((-1, 3)), rotation_matrix)
+    return rotated_data
+
+
+def jitter_point_cloud(batch_data, sigma=0.01, clip=0.05):
+    """ Randomly jitter points. jittering is per point.
+        Input:
+          BxNx3 array, original batch of point clouds
+        Return:
+          BxNx3 array, jittered batch of point clouds
+    """
+    B, N, C = batch_data.shape
+    assert(clip > 0)
+    jittered_data = np.clip(sigma * np.random.randn(B, N, C), -1 * clip, clip)
+    jittered_data += batch_data
+    return jittered_data
+
+def pointnet_full(y, my_tags, test_ids):
+    # -------------------------------------------------------------------------
+    # Load data
+    desired_points = 1024
+    X = np.load('data/processed/pointnetdata.npy', allow_pickle=True)
+    
+    X_train = X[test_ids[:-int(np.ceil(X.shape[0]*0.25))]]
+    X_test = X[test_ids[-int(np.ceil(X.shape[0]*0.25)):]]
+    y_train = y[test_ids[:-int(np.ceil(y.shape[0]*0.25))]]
+    y_test = y[test_ids[-int(np.ceil(X.shape[0]*0.25)):]]
+
+    # Training set
+    train_points_r = X_train
+    train_labels_r = y_train
+
+    # Test set
+    test_points_r = X_test
+    test_labels_r = y_test
+
+    # label to categorical
+    from keras.utils import to_categorical
+    #y_test = to_categorical(y_test)
+    #y_train = to_categorical(y_train)
+    # Let's examine the data. 
+
+    print("Training shape: ", train_points_r.shape)
+    print("Test shape: \t", test_points_r.shape)
+
+    # -------------------------------------------------------------------------
+    # hyperparameter
+    # number of points in each sample
+    num_points = desired_points
+
+    # number of categories
+    k = 18
+
+    # define optimizer
+    opt = Adam(lr=0.001, decay=0.7)
+
+    max_epochs=25
+    batch_size=32
+    dropout_rate = 0.7
+
+    # -------------------------------------------------------------------------
+    ### POINTNET ARCHITECTURE
+
+    input_points = Input(shape=(num_points, 3))
+    x = Convolution1D(64, 1, activation='relu', input_shape=(num_points, 3))(input_points)
+    x = BatchNormalization()(x)
+    x = Convolution1D(128, 1, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Convolution1D(1024, 1, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling1D(pool_size=num_points)(x)
+    x = Dense(512, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dense(256, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dense(9, weights=[np.zeros([256, 9]), np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32)])(x)
+    input_T = Reshape((3, 3))(x)
+
+    # For affine transformation need to matrix multiply
+    # forward net
+    g = Lambda(mat_mul, arguments={'B': input_T})(input_points)
+    g = Convolution1D(64, 1, input_shape=(num_points, 3), activation='relu')(g)
+    g = BatchNormalization()(g)
+    g = Convolution1D(64, 1, input_shape=(num_points, 3), activation='relu')(g)
+    g = BatchNormalization()(g)
+
+    # feature transform net
+    f = Convolution1D(64, 1, activation='relu')(g)
+    f = BatchNormalization()(f)
+    f = Convolution1D(128, 1, activation='relu')(f)
+    f = BatchNormalization()(f)
+    f = Convolution1D(1024, 1, activation='relu')(f)
+    f = BatchNormalization()(f)
+    f = MaxPooling1D(pool_size=num_points)(f)
+    f = Dense(512, activation='relu')(f)
+    f = BatchNormalization()(f)
+    f = Dense(256, activation='relu')(f)
+    f = BatchNormalization()(f)
+    f = Dense(64 * 64, weights=[np.zeros([256, 64 * 64]), np.eye(64).flatten().astype(np.float32)])(f)
+    feature_T = Reshape((64, 64))(f)
+
+
+    # forward net
+    g = Lambda(mat_mul, arguments={'B': feature_T})(g)
+    g = Convolution1D(64, 1, activation='relu')(g)
+    g = BatchNormalization()(g)
+    g = Convolution1D(128, 1, activation='relu')(g)
+    g = BatchNormalization()(g)
+    g = Convolution1D(1024, 1, activation='relu')(g)
+    g = BatchNormalization()(g)
+
+
+    # global_feature
+    global_feature = MaxPooling1D(pool_size=num_points)(g)
+
+
+    # point_net_cls
+    c = Dense(512, activation='relu')(global_feature)
+    c = BatchNormalization()(c)
+    c = Dropout(rate=dropout_rate)(c)
+    c = Dense(256, activation='relu')(c)
+    c = BatchNormalization()(c)
+    c = Dropout(rate=dropout_rate)(c)
+    c = Dense(k, activation='sigmoid')(c)
+    prediction = Flatten()(c)
+
+
+    # print the model summary
+    model = Model(inputs=input_points, outputs=prediction)
+    print(model.summary())
+
+
+    # compile classification model
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    # ------------------------------------------------------------------------------
+    # Fit model on training data
+    for i in range(1,max_epochs+1):
+        # model.fit(train_points_r, Y_train, batch_size=32, epochs=1, shuffle=True, verbose=1)
+        # rotate and jitter the points
+        train_points_rotate = rotate_point_cloud(train_points_r)
+        train_points_jitter = jitter_point_cloud(train_points_rotate)
+        history = model.fit(train_points_jitter, y_train, batch_size=batch_size, epochs=1,\
+                        shuffle=True, verbose=0, validation_split=0.1)
+        s = "Current epoch is:" + str(i)
+        print(s)
+        if i % 5 == 0:
+            score = model.evaluate(test_points_r, y_test, verbose=1)
+            print('Test loss: ', score[0])
+            print('Test accuracy: ', score[1])
+
+
+    # ## 10. Evaluate the Model
+    # score the model
+    score = model.evaluate(test_points_r, y_test, verbose=1)
+    print('Test loss: ', score[0])
+    print('Test accuracy: ', score[1])
+
+    print("###################################################################")
+    print("\nResults:\n")
+    # ------------------------------------------------------------------------------
+    # Classification Report
+
+    # make predictions on the test set
+    y_pred = model.predict(X_test)
+
+    ################################################################################
+    print("\n###################### Model Performance ############################")
+    # evaluate the model
+    _, train_acc = model.evaluate(X_train, y_train, verbose=0)
+    _, test_acc = model.evaluate(X_test, y_test, verbose=0)
+    print('\nTrain: %.3f, Test: %.3f' % (train_acc, test_acc))
+    ################################################################################
+    print("\n#####################################################################")
+
+    #Classification report
+    report = classification_report(y_test, y_pred.round(), target_names=my_tags)
+    print("\nClassfication Report for test:\n", report)
+    print("\n#####################################################################")
+
+    return(model, report)
